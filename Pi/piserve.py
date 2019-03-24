be9790425d864dd323e1571e092a8beeda77e5ad
TylersDurden <srobbin1@ithaca.edu>
2019-03-24 18:03:11 -0400

    pi play

--- Pi/piserve.py
+++ Pi/piserve.py
@@ -20,19 +20,21 @@ class Server:
     EVENT_TABLE = {'START': session_key}
 
     def __init__(self, run_time, timeout):
-        sock = self.initialize(run_time, timeout)
-        self.run(sock)
+        self.initialize(run_time)
+        self.run(timeout)
 
-    def initialize(self,run_time, timeout):
+    def initialize(self,run_time):
         if run_time == -1:
             self.looping = True
             self.run_time = 1e6
         else:
             self.run_time = run_time
-        sock = self.start_server(timeout)
-        return sock
 
-    def run(self, server_sock):
+    def run(self, timeout):
+        self.START = time.time()
+        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_sock.bind((self.bind_addr, self.port))
+        server_sock.listen(timeout)
         while (time.time() - self.START) <= self.run_time:
             try:
                 client, address = server_sock.accept()
@@ -43,15 +45,9 @@ class Server:
         server_sock.close()
         return 0
 
-    def start_server(self, timeout):
-        self.START = time.time()
-        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_sock.bind((self.bind_addr, self.port))
-        server_sock.listen(timeout)
-        return server_sock
-
     def request_handler(self, client):
         request = client.recv(1024)
+        print request
         if str(request) in self.EVENT_TABLE.keys():
             client.send(self.EVENT_TABLE[str(request)])
         client.close()
